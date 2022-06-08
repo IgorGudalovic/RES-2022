ecfb143c5835ec0c4d5b101fed47f4f933bc9617
Milicaaaa17 <101801318+Milicaaaa17@users.noreply.github.com>
2022-06-08 13:04:29 +0200

    Delete utils directory

--- utils/connection.py
+++
@@ -1,7 +0,0 @@
-from mysql import connector
-
-
-class Connector:
-    @staticmethod
-    def GetConnection():
-        return connector.connect(host='localhost', user='user', passwd='passwd', database='mysql')
