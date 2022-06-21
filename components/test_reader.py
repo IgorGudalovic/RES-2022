17745ab4b015820dbc9a9b8525936d81fbd4fee3
lazarevicpr47 <105983262+lazarevicpr47@users.noreply.github.com>
2022-06-21 21:19:50 +0200

    socket testovi dodati

--- components/test_reader.py
+++ components/test_reader.py
@@ -10,13 +10,29 @@ from models.item import Item
 import socket
 
 class TestReader(unittest.TestCase):
+    
     def test_codeselect(self):
         with mock.patch('builtins.input', return_value="1"):
             assert Reader.codeSelectionFunction() == "1"\
                 or Reader.codeSelectionFunction() == "2"\
                     or Reader.codeSelectionFunction() == "3"\
                         or Reader.codeSelectionFunction() == "4"   
-         
+
+    def test_server_ok(self):        
+            socket1 = Reader.CreateServerSocket()
+            self.assertEqual(type(socket.socket()), type(socket1))
+
+    def test_server_bad(self):        
+        socket1 = Reader.CreateServerSocket()
+        self.assertNotEqual(type(socket.socket()), type(socket1))    
+
+    def test_client_ok(self):        
+        socket1 = Reader.CreateClientSocket()
+        self.assertEqual(type(socket.socket()), type(socket1))
+
+    def test_client_bad(self):        
+        socket1 = Reader.CreateClientSocket()
+        self.assertNotEqual(type(socket.socket()), type(socket1))          
         
 if __name__ == '__main__':
     unittest.main()        
